644ce32f6e0ea7a4b535b8fee2b119815c7d08a6
bquinn <brendan@cluefulmedia.com>
2018-01-08 16:54:50 +0000

    Update PyPI setup file.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pats',
   packages = ['pats'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Client for the PATS API (www.pats.org.uk)',
   author = 'Brendan Quinn',
   author_email = 'brendan@cluefulmedia.com',
   url = 'https://github.com/bquinn/pats-api-python',
-  download_url = 'https://github.com/bquinn/pats-api-python/0.1',
+  download_url = 'https://github.com/bquinn/pats-api-python/archive/0.3.tar.gz',
   keywords = ['api', 'publishing', 'advertising'],
   classifiers = [],
 )
