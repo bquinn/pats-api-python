e8aa7b1e4e153e0220cf87d0432588b4774779f1
bquinn <brendan@cluefulmedia.com>
2018-01-08 17:22:28 +0000

    More changes to setup file for latest PyPI compatibility

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'pats',
-  packages = ['pats'], # this must be the same as the name above
+  packages=find_packages(exclude=['contrib', 'docs', 'tests']),
   version = '0.3',
   description = 'Client for the PATS API (www.pats.org.uk)',
+  long_description=long_description,
   author = 'Brendan Quinn',
   author_email = 'brendan@cluefulmedia.com',
   url = 'https://github.com/bquinn/pats-api-python',
   download_url = 'https://github.com/bquinn/pats-api-python/archive/0.3.tar.gz',
   keywords = ['api', 'publishing', 'advertising'],
-  classifiers = [],
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
 )
